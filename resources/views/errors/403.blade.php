a8b03cf54736c91efcc3629309b8ee4fd9d35b1e
Singee <imsingee@gmail.com>
2018-07-31 10:24:52 +0800

    403 improved

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -5,7 +5,7 @@
     <div class="jumbotron">
         <h1>403 错误</h1>
         <p class="lead">
-            {{ $exception->getMessage() ? $exception->getMessage(): "禁止访问 Permission Denied" }}
+            {{ $exception->getMessage() ?: "禁止访问 Permission Denied" }}
         </p>
         <p>
             <a class="btn btn-lg btn-success" href="{{ route('home') }}" role="button">返回首页</a>
